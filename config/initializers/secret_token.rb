a0b9319befd51bc2f824e210432e4b990f4e84a5
nlfonseca <nelson.fonseca@tekzenit.com>
2014-06-20 19:43:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flickr::Application.config.secret_key_base = 'a5200543b8428228874a92bb51e5b285ff6e4ff134b03f39543a7980296e1d040a703baf1c470d24e75a5ab528758e6b6b5731c41e8363f0bf8064f28312ec65'
